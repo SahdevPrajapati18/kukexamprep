78832b3c0005d64b7f3cc610860cb73777b6aae8
SahdevPrajapati18 <Sahdevprajapati27@gmail.com>
2025-06-16 20:28:46 +0530

    removing gradle.properties

---
+++ app/build/generated/source/buildConfig/debug/com/kuk/kukexamprep/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kuk.kukexamprep;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.kuk.kukexamprep";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndycGNqcWZuc2RjZGh6dHZtbGZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2MTI0NzAsImV4cCI6MjA2MjE4ODQ3MH0.1Fxb-T7yKla7aUkG-_8PDxwD4CaY3e1YihKE21a-X34";
+}
