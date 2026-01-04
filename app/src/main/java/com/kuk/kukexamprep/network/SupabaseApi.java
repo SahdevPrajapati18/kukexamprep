198949927fc494bb297850257ae6a7d83bb67760
Sahdev <Sahdevprajapati27@gmail.com>
2026-01-04 19:52:28 +0530

    api key changes

--- app/src/main/java/com/kuk/kukexamprep/network/SupabaseApi.java
+++ app/src/main/java/com/kuk/kukexamprep/network/SupabaseApi.java
@@ -9,8 +9,8 @@ import retrofit2.http.Headers;
 
 public interface SupabaseApi {
     @Headers({
-            "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2enNvdm5rcnRyd3FrcXpqZXVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NzY1OTEsImV4cCI6MjA2MjA1MjU5MX0.eFb5nr-KKx81Hagu203PeQh0GpADo7g7N1Bp-G0sqkY",
-            "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx2enNvdm5rcnRyd3FrcXpqZXVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NzY1OTEsImV4cCI6MjA2MjA1MjU5MX0.eFb5nr-KKx81Hagu203PeQh0GpADo7g7N1Bp-G0sqkY"
+            "apikey: sb_publishable_c6KvkGC_HH8g5e5N7Bd8Ew_wpNoq9V7",
+            "Authorization: Bearer sb_publishable_c6KvkGC_HH8g5e5N7Bd8Ew_wpNoq9V7"
     })
     @GET("rest/v1/notes?select=*")  // Replace `pdfs` with your actual table name
     Call<List<PdfItem>> getPdfs();
